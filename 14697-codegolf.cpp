9cddf3950cab039098eb57bcca52bb748afb973d
Yangsa <sat0317@naver.com>
2020-12-11 09:43:33 +0900

    Create 14697-codegolf.cpp

---
+++ 14697-codegolf.cpp
@@ -0,0 +1,1 @@
+main(i,j,k,a,b,c,n){scanf("%d%d%d%d",&a,&b,&c,&n);if(n%a==0||n%b==0||n%c==0){printf("1");return 0;}for(i=0;i<=n;i+=a)for(j=0;j<=n;j+=b)for(k=0;k<=n;k+=c)if(n==i+j+k){printf("1");return 0;}printf("0");}
