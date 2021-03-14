c86c72872cb2ad251037b2b647789ae98b084a40
Yangsa <sat0317@naver.com>
2021-03-15 08:16:27 +0900

    Create 2442.cpp

---
+++ 2442.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, j, k, n;
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        for(j=0;j<n-i;j++){
+            printf(" ");
+        }
+        for(j=0;j<i*2-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
