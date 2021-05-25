20dd5ef61eb20bf20471b00dfb5bbf29b6459455
Yangsa <sat0317@naver.com>
2021-05-26 08:51:56 +0900

    Update 10814.cpp

--- 10814.cpp
+++ 10814.cpp
@@ -1,49 +1,27 @@
 #include <stdio.h>
+#include <algorithm>
 
-int year[100000], whi[100000];char name[100000][101];
+using namespace std;
 
-//void sort(int a[], int l, int r){
-//	int pb=a[l];
-//	int p=l+1;
-//	int q=r, t;
-//	
-//	if(l>=r) return;
-//	
-//	while(1){
-//		while(a[p]<=pb && p<r) p++;
-//		while(a[q]>pb && q>l) q--;
-//		
-//		if(p<q){
-//			t=a[p];a[p]=a[q];a[q]=t;
-//			t=whi[p];whi[p]=whi[q];whi[q]=t;
-//		} else {
-//			t=a[l];a[l]=a[q];a[q]=t;
-//			t=whi[l];whi[l]=whi[q];whi[q]=t;
-//			break;
-//		}
-//	}
-//	
-//	sort(a, l, q-1);
-//	sort(a, q+1, r);
-//}
+struct person{
+	int age;
+	char name[101]={};
+}; 
 
-void sort(int a[], int n){
-	
+bool sos(person a, person b){
+	return a.age<b.age;
 }
 
 int main(){
-	int n;
+	int n; person p[100001];
 	scanf("%d", &n);
-	
 	for(int i=0;i<n;i++){
-		scanf("%d %s", &year[i], &name[i]);
-		whi[i]=i;
+		scanf("%d %s", &p[i].age, p[i].name);
 	}
-	
-	sort(year, 0, n-1);
+	stable_sort(p, p+n, sos);
 	
 	for(int i=0;i<n;i++){
-		printf("%d %s\n", year[i], name[whi[i]]);
+		printf("%d %s\n", p[i].age, p[i].name);
 	}
 	
 	return 0;
