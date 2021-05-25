d86c22e7d744e026b126b88c3ca3a8322e53a2d0
Yangsa <sat0317@naver.com>
2021-05-26 08:52:45 +0900

    Update 1181.cpp

--- 1181.cpp
+++ 1181.cpp
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-	char a[20000][52]={};
+#include <algorithm>
 
-int qsar(int left, int right){
-	int i = left, j = right;
-    int pivot = arr[(left + right) / 2];
-    int temp;
-    do {
-        while(arr[i] < pivot)
-            i++;
-        while(arr[j] > pivot)
-            j--;
-        if (i<=j)
-        {
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-        }
-    } while (i<= j);
+struct word{
+	int len;
+	char wo[51]={};
+	bool cans;
+};
 
-    if (left < j)
-        quickSort(left, j);
-
-    if (i < right)
-        quickSort(i, right);
+bool sos(word a, word b){
+	if(a.len==b.len){
+		for(int i=0;i<a.len;i++){
+			if(a.wo[i]!=b.wo[i]){
+				return a.wo[i]<b.wo[i];
+			}
+		}
+	} else {
+		return a.len<b.len;
+	}
 }
 
 int main(){
 	int n;
-	
+	word a[20001];
 	scanf("%d", &n);
-	for(i=0;i<n;i++){
-		scanf("%s", a[i]);
-		a[i][52]=strlen(a[i]);
+	for(int i=0;i<n;i++){
+		scanf("%s", a[i].wo);
+		a[i].len=strlen(a[i].wo);
+		a[i].cans=true;
+		for(int j=0;j<i;j++){
+			if(a[i].len!=a[j].len) continue;
+			if(!strcmp(a[i].wo, a[j].wo)) {
+				a[i].cans=false;
+				break;
+			}
+		}
+	}
+	std::sort(a, a+n, sos);
+	for(int i=0;i<n;i++){
+		if(a[i].cans){
+			printf("%s\n", a[i].wo);
+		}
 	}
 	
 	return 0;
